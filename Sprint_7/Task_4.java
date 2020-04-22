839ada59f3ab197e9bc46bb976fd6909f2b678e0
KhrystynaVysotska <56559854+KhrystynaVysotska@users.noreply.github.com>
2020-04-22 19:35:34 +0300

    Create Task_4.java

---
+++ Sprint_7/Task_4.java
@@ -0,0 +1,7 @@
+public enum LineType {
+    SOLID, DOTTED, DASHED, DOUBLE
+}
+
+ public static String drawLine(LineType lineType) {
+        return "The line is " + lineType.name().toLowerCase() + " type";
+    }
