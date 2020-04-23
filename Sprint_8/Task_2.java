2d1d7df6aeaedbb63ef9d945fd7ca8f3f20b59c7
KhrystynaVysotska <56559854+KhrystynaVysotska@users.noreply.github.com>
2020-04-23 18:20:31 +0300

    Update Task_2.java

--- Sprint_8/Task_2.java
+++ Sprint_8/Task_2.java
@@ -2,19 +2,14 @@ import java.util.function.Consumer;
 import java.util.Arrays;
 public class App {
 
-    static Consumer<double[]> cons = new Consumer<double[]>() {
-
-        @Override
-        public void accept(double[] myDoubleArray) {
-            for(int i = 0; i < myDoubleArray.length; i++) {
-                if(myDoubleArray[i] > 2) {
-                    myDoubleArray[i] *= 0.8;
-                } else {
-                    myDoubleArray[i] *= 0.9;
-                }
+    static Consumer<double[]> cons = (array) -> {
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > 2) {
+                array[i] *= 0.8;
+            } else {
+                array[i] *= 0.9;
             }
-            
-        }         
+        }
     };
     
     public static double[] getChanged(double[] initialArray, Consumer<double[]> consumer) {
